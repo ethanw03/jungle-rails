071f4d32211cb6e525a50d85af13cbc527bb4e1a
Ethan Wong <ethanwong98@yahoo.ca>
2023-03-10 05:05:45 -0800

    created about page, controller, and routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  resource :about, only: [:show], controller: "about"
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
